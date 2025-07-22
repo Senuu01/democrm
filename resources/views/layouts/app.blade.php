88905a0996ec4f9c5cace634469248cdbb962947
Senuu01 <senuthdamjaya55@gmail.com>
2025-07-22 12:47:40 +0530

    Fix CSS for login/register pages - add CDN Tailwind to all layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
         
         <!-- Dark Mode Script -->
         <script>
